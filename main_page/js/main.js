5daec286f5b79804bb80103943314314053c90e9
楊鈞皓 <john705810420@gmail.com>
2021-08-30 02:02:49 +0800

    動態牆更新：

--- main_page/js/main.js
+++ main_page/js/main.js
@@ -1,58 +1,70 @@
-var prevScrollpos = window.pageYOffset;
-window.onscroll = function() {
-  var currentScrollPos = window.pageYOffset;
-  if (prevScrollpos > currentScrollPos) {
-    document.querySelector(".nav-outer").style.top = "0";
-    document.querySelector(".nav-outer").style.opacity = "1";
-  } else {
-    document.querySelector(".nav-outer").style.top = "-60px";
-    document.querySelector(".nav-outer").style.opacity = "0";
+document.addEventListener('DOMContentLoaded', function(){
+
+  var prevScrollpos = window.pageYOffset;
+  const sec = document.querySelectorAll(".section");
+  const menuButton = document.querySelector(".menu-buttom");
+  const menuList = document.querySelector(".menu-list");
+
+  const displayScrollElement = (element) => {
+    element.classList.add("add-animate");
+  };
+
+  const hideScrollElement = (element) => {
+    element.classList.remove("add-animate");
+  };
+
+  window.addEventListener("onresize", () => {
+    menuButton.querySelector(".ham").classList.remove("menu-open");;
+    menuList.classList.remvoe("nav-list-open");
+    let widthElement = window.innerWidth;
+    if (innerWidth >= 768){
+
+    }
+    console.log("aaaa");
+  });
+
+  window.onscroll = function() {
+    let currentScrollPos = window.pageYOffset;
+    if (prevScrollpos > currentScrollPos) {
+      document.querySelector(".nav-outer").style.top = "0";
+      document.querySelector(".nav-outer").style.opacity = "1";
+    } else {
+      document.querySelector(".nav-outer").style.top = "-60px";
+      document.querySelector(".nav-outer").style.opacity = "0";
+    }
+    prevScrollpos = currentScrollPos;
   }
-  prevScrollpos = currentScrollPos;
-}
 
-const sec = document.querySelectorAll(".section");
-
-var getScroll = [];
-
-const displayScrollElement = (element) => {
-  element.classList.add("add-animate");
-};
-
-const hideScrollElement = (element) => {
-  element.classList.remove("add-animate");
-};
-
-// getSectionByScroll(getScroll);
-handleAnimation();
-window.addEventListener("scroll", () => {
   handleAnimation();
-});
+  window.addEventListener("scroll", () => {
+    handleAnimation();
+    console.log("a");
+  });
 
-// function getSectionByScroll(getHeight) {
-//   let secNum = sec.length;
-//   for (let i =0;i<secNum;i++){
-//     getHeight[i] = sec[i].scrollTop;
-//   }
-//   console.log(secNum);
-// }
-console.log(sec);
-function handleAnimation() {
-  document.querySelectorAll(".section").forEach((el) => {
-    getSectionHeight(el);
-  })
-  console.log("a");
-}
-function getSectionHeight(el) {
-  let getTop = el.getBoundingClientRect().top;
-  let getHeight = el.getBoundingClientRect().height / 3;
-  let getBottom = el.getBoundingClientRect().bottom;
-  if (getTop < getHeight && getBottom > getHeight) {
-    displayScrollElement(el);
+  function handleAnimation() {
+    sec.forEach((el) => {
+      getSectionHeight(el);
+    })
   }
-  else {
-    hideScrollElement(el);
+  function getSectionHeight(el) {
+    let getTop = el.getBoundingClientRect().top;
+    let getHeight = el.getBoundingClientRect().height;
+    let getBottom = el.getBoundingClientRect().bottom;
+    if (getTop < getHeight && getBottom > 0) {
+      displayScrollElement(el);
+    }
+    else {
+      hideScrollElement(el);
+    }
+    console.log(getTop);
   }
-  console.log("getTop = "+getTop);
-  console.log("getBottom = "+getBottom);
-}
+  /*navbar*/
+
+  menuButton.addEventListener("click", () => {
+    this.querySelector(".ham").classList.toggle("menu-open");
+    menuList.classList.toggle("nav-list-open");
+  });
+
+  /*navbar end*/
+
+});
